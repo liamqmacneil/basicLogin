40f0adebfac71928e7ce44c55be947c9d7ebcef9
Liam MacNeil <32491405+DakKhuza@users.noreply.github.com>
2018-06-12 11:23:16 -0700

    Comments + Removed some unused features

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
+    //Handle adding css for the fancy on click animation
     $(".input").focus(function() {
         $(".fancyBorder").addClass("fancyBorderFocus")
         $(".fancyEmail").addClass("fancyTextAnim")
         $(".fancyPassword").addClass("fancyTextAnim")
     })
-    
+    //Handle removing css for the fancy on click animation
     $(".input").focusout(function() {
         if ($(this).val() === '') {
             $(".fancyEmail").removeClass("fancyTextAnim")
@@ -12,51 +13,50 @@ $(document).ready(function() {
             $(".fancyPassword").removeClass("fancyTextAnim")
         }
     })
-
+    //Handle clicking on areas of input box blocked by other elements
     $(".fancyEmail").click(function() {
         $(".email").focus();
         $(".fancyText").addClass("fancyTextAnim")
     })
-
+    //Handle clicking on areas of input box blocked by other elements
+    //but this time for password input
     $(".fancyPassword").click(function() {
         $(".password").focus();
         $(".fancyPassword").addClass("fancyTextAnim")
     })
 
-    $(".input").keyup( function() {
-        $(".fancyInputFade").value = $(".email").val();
-    })
-
+    //The form handling code
     $(".formInput").submit(function(e) {
-        e.preventDefault();
-        var emailRegex = new RegExp('.+@.+\..+');
+        e.preventDefault(); //self explanatory
+        var emailRegex = new RegExp('.+@.+\..+'); //Create regex
 
-        if ($('.password').css('display') == 'none') {
-            if (emailRegex.test($('.email').val())) {
-                emailSideOut()
+        if ($('.password').css('display') == 'none') { //Check if password or email being input
+            if (emailRegex.test($('.email').val())) { //check if meets regex
+                emailSideOut(); //Fancy email slide out
             }else {
-                showError('Invalid Email');
+                showError('Invalid Email'); //Show error message to use
             }
         }else {
-            loginCall($(this).serialize());
+            loginCall($(this).serialize()); //Calls ajax with form input
         }
 
         function loginCall(formData) {
-            $.ajax({
+            $.ajax({ //Call php function
                 type: "POST",
                 url: "login.php",
                 data: formData,
                 datatype:'json',
                 success: function(data) {
-                    console.log(data);
-                    data = JSON.parse(data);
-                    if (data.status == 0) {
-                        showError(data.payload);
+                    data = JSON.parse(data); //Parse data into JSON for ease of use
+                    if (data.status == 0) { //Check if valid on server side
+                        showError(data.payload); //Show error
                     }else {
                         $(".shadow").addClass("fadeOut");
+                        //Fancy css effects \/ /\
                         $(".loginContent").css('display','block');
-                        $(".loginContent").html(data.payload)
-                        $('.loginContent').addClass('fadeIn');
+
+                        $(".loginContent").html(data.payload); //Update login content with login content
+                        $('.loginContent').addClass('fadeIn'); //Fade it in
                     }
                 }
             })
@@ -64,17 +64,17 @@ $(document).ready(function() {
 
 
         function showError(err) {
-            $(".errorMsg").css("display","block")
-            $(".errorMsg").html(err);
+            $(".errorMsg").css("display","block"); //Display error div
+            $(".errorMsg").html(err); //Set error div content
         }
 
         function emailSideOut() {
-            $(".email").css("display","none");
-            $(".password").css("display","block");
-            $(".password").focus();
-            $(".errorMsg").css("display","none")
-            $(".fancyEmail").addClass("fancyTextSlideOut")
-            $(".fancyPassword").addClass("fancyTextSlideIn")
+            $(".email").css("display","none"); //hide email input
+            $(".password").css("display","block"); //Display password input
+            $(".password").focus(); //Focus on password input
+            $(".errorMsg").css("display","none");//hide error message
+            $(".fancyEmail").addClass("fancyTextSlideOut");//Side fancy email text out
+            $(".fancyPassword").addClass("fancyTextSlideIn");//Slide fancy password text in
         }
     })
 })
