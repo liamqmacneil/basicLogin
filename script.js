6aacfcfcfe5a55d62d707953982a227b8ef5803f
Liam MacNeil <32491405+DakKhuza@users.noreply.github.com>
2018-06-12 11:13:50 -0700

    JS update
    Added JS to handle login + some fancy css effects

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
     $(".email").focus(function() {
+    $(".input").focus(function() {
         $(".fancyBorder").addClass("fancyBorderFocus")
         $(".fancyEmail").addClass("fancyTextAnim")
         $(".fancyPassword").addClass("fancyTextAnim")
     })
-    $(".email").focusout(function() {
-        if ($(".email").val() === '') {
+    
+    $(".input").focusout(function() {
+        if ($(this).val() === '') {
             $(".fancyEmail").removeClass("fancyTextAnim")
             $(".fancyBorder").removeClass("fancyBorderFocus")
             $(".fancyPassword").removeClass("fancyTextAnim")
@@ -18,29 +20,62 @@ $(document).ready(function() {
     })
 
     $(".fancyPassword").click(function() {
-        $(".email").focus();
+        $(".password").focus();
         $(".fancyPassword").addClass("fancyTextAnim")
+    })
+
+    $(".input").keyup( function() {
+        $(".fancyInputFade").value = $(".email").val();
     })
 
     $(".formInput").submit(function(e) {
         e.preventDefault();
-        $.ajax({
-            type: "POST",
-            url: "login.php",
-            data: $( this ).serialize(),
-            datatype:'json',
-            success: function(data) {
-                if (data === 'login') {
-                    console.log("Login!!")
-                    $(".errorMsg").css("display","none")
-                    $(".fancyEmail").addClass("fancyTextSlideOut")
-                    $(".fancyPassword").addClass("fancyTextSlideIn")
-                }else {
-                    $(".errorMsg").css("display","block")
-                    $(".errorMsg").addClass("errorFlash")
-                    console.log(data)
-                }
+        var emailRegex = new RegExp('.+@.+\..+');
+
+        if ($('.password').css('display') == 'none') {
+            if (emailRegex.test($('.email').val())) {
+                emailSideOut()
+            }else {
+                showError('Invalid Email');
             }
-        })
+        }else {
+            loginCall($(this).serialize());
+        }
+
+        function loginCall(formData) {
+            $.ajax({
+                type: "POST",
+                url: "login.php",
+                data: formData,
+                datatype:'json',
+                success: function(data) {
+                    console.log(data);
+                    data = JSON.parse(data);
+                    if (data.status == 0) {
+                        showError(data.payload);
+                    }else {
+                        $(".shadow").addClass("fadeOut");
+                        $(".loginContent").css('display','block');
+                        $(".loginContent").html(data.payload)
+                        $('.loginContent').addClass('fadeIn');
+                    }
+                }
+            })
+        }
+
+
+        function showError(err) {
+            $(".errorMsg").css("display","block")
+            $(".errorMsg").html(err);
+        }
+
+        function emailSideOut() {
+            $(".email").css("display","none");
+            $(".password").css("display","block");
+            $(".password").focus();
+            $(".errorMsg").css("display","none")
+            $(".fancyEmail").addClass("fancyTextSlideOut")
+            $(".fancyPassword").addClass("fancyTextSlideIn")
+        }
     })
 })
