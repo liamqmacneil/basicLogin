8a812a761997581939c1342cd0aa2c2aeea4a45f
Liam MacNeil <32491405+DakKhuza@users.noreply.github.com>
2018-06-12 11:13:20 -0700

    PHP updated
    Added all code to handle simple login

--- login.php
+++ login.php
@@ -1,10 +1,26 @@
 <?php
+    $dataObj = new \stdClass();
     if (isset($_POST['email'])) {
-        $email = $_POST['email'];
-        if (preg_match('/.+\@.+\./',$email)) {
-            echo "login";
-        }else {
-            echo "invalid";
+        if (isset($_POST['password'])) {
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            if (preg_match('/.+\@.+\..+/',$email)) {
+                //I'd use a DB connection here but I feel
+                //thats too complex and would make testing
+                //annoying, I'd use something like mongodb
+                if ($email == 'hr@auphansoftware.com') {
+                    if ($password == 'hello') {
+                        $dataObj->status = '1';
+                        $dataObj->payload = 'Login Successful.';
+                        echo json_encode($dataObj);
+                        exit();
+                        
+                    }
+                }
+            }
         }
     }
+    $dataObj->status = '0';
+    $dataObj->payload = 'Invalid email and/or password.';
+    echo json_encode($dataObj);
 ?>
