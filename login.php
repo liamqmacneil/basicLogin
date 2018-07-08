d03bc4132830f4b255af9db6fc27fe1414e29fa6
Liam <steam.name.mailcam2a@gmail.com>
2018-07-08 06:52:46 -0700

    Update login.php

--- login.php
+++ login.php
@@ -8,8 +8,8 @@
                 //I'd use a DB connection here but I feel
                 //thats too complex and would make testing
                 //annoying, I'd use something like mongodb
-                if ($email == 'hr@auphansoftware.com') {
-                    if ($password == 'hello') {
+                if ($email == 'email@email.com') {
+                    if ($password == 'password') {
                         $dataObj->status = '1';
                         $dataObj->payload = 'Login Successful.';
                         echo json_encode($dataObj);
